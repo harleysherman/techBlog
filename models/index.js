89cd965234b80d987de3aee88a8e3abd8e524ea1
harleysherman <harsatr11@icloud.com>
2024-02-13 12:41:32 -0600

    added prettier and starter code from mini project

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Project = require('./BlogPost');
+
+User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Project };
