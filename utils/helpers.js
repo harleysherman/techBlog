5612f42d848b13927f57062225a75924f8fb5fbd
harleysherman <harsatr11@icloud.com>
2024-02-25 20:26:05 -0600

    added styles and fixed adding comments to a post

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,6 @@
 module.exports = {
-  format_date: (date) => {
+  formatDate: (date) => {
     // Format date as MM/DD/YYYY
-    const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    console.log("date::", date);
-    return new Date(date).toLocaleDateString(undefined, options);
-  }
+    return date.toLocaleDateString();
+  },
 };
